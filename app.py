b7cf50b9a76e4d05a402888b2f0c7b5f7aadbf39
JimLeaf <35558488+JimLeaf@users.noreply.github.com>
2025-12-05 07:07:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
-# Read your timeline HTML file
-html_code = open("index_inline.html").read()
+with open("index_inline.html", "r", encoding="utf-8") as f:
+    html = f.read()
 
-# Render it full width
-st.components.v1.html(
-    html_code,
-    height=6150,       # adjust height as needed
-    scrolling=False
-)
+st.components.v1.html(html, height=6150, scrolling=False)
 
-st.markdown("""
-<style>
-iframe {
-    width: 100% !important;   /* force iframe to span full width */
-}
-</style>
-""", unsafe_allow_html=True)
 
 
 
