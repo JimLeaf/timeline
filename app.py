654b298840704d701782f2884573c0ef7a98299f
JimLeaf <35558488+JimLeaf@users.noreply.github.com>
2025-12-05 07:12:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,7 @@ with open("index_inline.html", "r", encoding="utf-8") as f:
 
 st.components.v1.html(html, height=6150, scrolling=False)
 
-st.markdown("""
-<script>
-const iframe = document.querySelector("iframe");
-window.addEventListener("scroll", () => {
-  const scrollTop = window.scrollY || document.documentElement.scrollTop;
-  if (iframe && iframe.contentWindow) {
-    iframe.contentWindow.postMessage({ type: "parentScroll", scrollTop }, "*");
-  }
-});
-</script>
-""", unsafe_allow_html=True)
+
 
 
 
