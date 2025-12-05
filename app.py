bf48ea3187c884430f7a436501f98ddae1776c13
JimLeaf <35558488+JimLeaf@users.noreply.github.com>
2025-12-05 06:51:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ st.set_page_config(layout="wide")
 with open("index_inline.html", "r", encoding="utf-8") as f:
     html = f.read()
 
-st.components.v1.html(html, height=6150, scrolling=True)
+st.components.v1.html(html, height=6150, scrolling=False)
+
 
 
 
